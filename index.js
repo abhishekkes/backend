bd8af667354e54bbcb7e27bfa43f40b7cd3de5d5
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-01-10 11:30:15 +0530

    updated

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.post("/api/users", (req, res) => {
   });
 });
 
+app.patch("/api/users",()=>{
+  console.log("Patch ")
+})
+
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
